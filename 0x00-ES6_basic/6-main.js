8682c5db9dd3f819051367e9e354ce0c2b42f838
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-04-26 01:30:04 +0300

    feat: Rewrite the return statement to use a template literal so you can the substitute the variables you’ve defined.

---
+++ 0x00-ES6_basic/6-main.js
@@ -0,0 +1,3 @@
+import getSanFranciscoDescription from './6-string-interpolation.js';
+
+console.log(getSanFranciscoDescription(2022, { income: '200,000', gdp: '$300 billion', capita: '$300,000' }));
