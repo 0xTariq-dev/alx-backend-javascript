e432d78a161433b18321ad28c5cae743bcc3f4bb
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-04-26 01:40:33 +0300

    fix: string interpolation task.

--- 0x00-ES6_basic/6-main.js
+++ 0x00-ES6_basic/6-main.js
@@ -1,3 +1,3 @@
 import getSanFranciscoDescription from './6-string-interpolation.js';
 
-console.log(getSanFranciscoDescription(2022, { income: '200,000', gdp: '$300 billion', capita: '$300,000' }));
+console.log(getSanFranciscoDescription());
