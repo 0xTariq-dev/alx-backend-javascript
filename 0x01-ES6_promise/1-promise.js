912982706fa2810b0c3a512fae29518225637270
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-04-29 21:27:14 +0300

    feat: function that returns a promise object.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const status = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return status;
+}
