0619f420100944b281d073c005051f9281412035
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-01 00:57:38 +0300

    fix: Basic list of objects task #2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,6 @@ export default function getListStudents() {
   const guillaume = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const james = { id: 2, firstName: 'James', location: 'Columbia' };
   const serena = { id: 3, firstName: 'Serena', location: 'San Francisco' };
-  const students = [guillaume, james, serena];
 
-  return students;
+  return [guillaume, james, serena];
 }
