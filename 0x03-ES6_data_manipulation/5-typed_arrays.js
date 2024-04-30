d974ec08e2acf7e6fda8d019d65987552e3c11c9
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-01 02:25:55 +0300

    feat: Create a function named setFromArray that returns a Set from an array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    const int8View = new Int8Array(buffer);
-    if (position >= length) return 'Position outside range';
-    int8View[position] = value;
-    const dataView = new DataView(buffer);
-    return dataView;
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  if (position >= length) throw Error('Position outside range');
+  int8View[position] = value;
+  const dataView = new DataView(buffer);
+  return dataView;
 }
