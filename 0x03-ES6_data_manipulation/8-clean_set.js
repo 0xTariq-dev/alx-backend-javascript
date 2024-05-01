51093981d10370f0beff3698b0b5d048f30f3bda
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-01 20:48:31 +0300

    fix: Clean set task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
-  return !startString ? '' : Array.from(set)
+  return (!startString || typeof startString !== 'string') ?
+  '' : Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.replace(startString, ''))
     .join('-');
