714481fad1425dbe057405f225640a4f093e8a16
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-07 22:27:54 +0300

    fix: return a simple resloved promise linting

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,11 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-        if (firstName && lastName) {
-            resolve(
-                { 'firstName': firstName, 'lastName': lastName }
-            );
-        } else {
-            reject(new Error('Missing parameters'));
-        }
-    })
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName) {
+      resolve(
+        { firstName, lastName },
+      );
+    } else {
+      reject(new Error('Missing parameters'));
+    }
+  });
 }
