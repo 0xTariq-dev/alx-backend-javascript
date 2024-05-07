c0021d6df1eea6b17ba8fcbd2c53a89db44d6953
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-05-08 00:02:40 +0300

    fix: Handle multiple promises in a function named handleProfileSignup #2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  const userPromise = signUpUser(firstname, lastname);
-  const photoPromise = uploadPhoto(filename);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
   return Promise.allSettled([userPromise, photoPromise])
     .then((res) => res);
 }
